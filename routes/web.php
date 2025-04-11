3dc5f504e9fc1ac65d8d2fb2dd5dbacf336f8663
tbd-shafa <shafa@technobd.com>
2025-04-11 16:13:19 +0600

    pagination in blog

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -13,14 +14,15 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
     Route::resource('blogs', BlogController::class);
     Route::patch('blogs/{blog}/update-status', [BlogController::class, 'updateStatus'])
-    ->name('blogs.update-status');
+        ->name('blogs.update-status');
+    Route::post('blogs/posts/{id}/review', [BlogController::class, 'storeReview'])->name('blogs.review');
+    Route::put('blogs/posts/{id}/review-update', [BlogController::class, 'updateReview'])->name('blogs.review.update');
 });
 // Public routes (no auth required)
 Route::get('blogs/posts', [BlogController::class, 'show'])->name('blogs.posts');
 Route::get('blogs/posts/{id}', [BlogController::class, 'details'])->name('blogs.details');
-Route::post('blogs/posts/{id}/review', [BlogController::class, 'storeReview'])->middleware(['auth'])->name('blogs.review');
-Route::put('blogs/posts/{id}/review-update', [BlogController::class, 'updateReview'])->middleware(['auth'])->name('blogs.review.update');
 
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
